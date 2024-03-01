2adbc1db1703b7d16b3cba493ec1de17567c7bf0
Aplle <theaplle@outlook.com>
2024-03-01 14:23:59 -0600

    v1.1

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 function botonAdd() {
-    alert("Se agregó a la bandeja");
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function (checkbox) {
-        checkbox.checked = false;
+    var comidas = document.querySelectorAll(".beverage");
+
+    var cantidadTotal = 0;
+    var costoTotal = 0;
+    var detallePedido = "";
+
+    comidas.forEach(function (comida) {
+        var inputCantidad = comida.querySelector('input[type="number"]');
+        var spanPrecio = comida.querySelector(".price");
+        var labelComida = comida.querySelector("label");
+
+        var nombreComida = labelComida.textContent;
+        var cantidad = parseInt(inputCantidad.value);
+        var precio = parseFloat(spanPrecio.textContent.replace("$", ""));
+
+        if (cantidad > 0) {
+            cantidadTotal += cantidad;
+            costoTotal += cantidad * precio;
+            detallePedido +=
+                cantidad +
+                "x " +
+                nombreComida +
+                " - $" +
+                (cantidad * precio).toFixed(2) +
+                "\n";
+        }
     });
+
+    var mensaje =
+        "Se agregó a la bandeja:\nCantidad total: " +
+        cantidadTotal +
+        "\nCosto total: $" +
+        costoTotal.toFixed(2) +
+        "\n\nDetalle del pedido:\n" +
+        detallePedido;
+    alert(mensaje);
+
     var numberInputs = document.querySelectorAll('input[type="number"]');
     numberInputs.forEach(function (number) {
         number.value = 0;
